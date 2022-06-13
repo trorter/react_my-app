caa986d4c3bd97c01330393833fc435e3c2a99f6
andrey <auledovskih@gmail.com>
2022-06-13 17:09:29 +0300

    add css updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,19 @@ import s from "./Navbar.module.css"
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div className={s.item}>
+            <div className={`${s.item} ${s.active}`}>
                 <a href={"blank"} >Profile</a>
             </div>
-            <div className={s.item}>
+            <div className={`${s.item} ${s.active}`}>
                 <a href={"blank"}>Messages</a>
             </div>
-            <div className={s.item}>
+            <div className={`${s.item} ${s.active}`}>
                 <a href={"blank"}>News</a>
             </div>
-            <div className={s.item}>
+            <div className={`${s.item} ${s.active}`}>
                 <a href={"blank"}>Music</a>
             </div>
-            <div className={s.item}>
+            <div className={`${s.item} ${s.active}`}>
                 <a href={"blank"}>Settings</a>
             </div>
         </nav>
