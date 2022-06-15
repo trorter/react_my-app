73c7ae001520ecc9f72532886dfe1fcb4536b5e1
andrey <auledovskih@gmail.com>
2022-06-15 22:30:19 +0300

    added move out BLL from UI part 2

--- src/App.js
+++ src/App.js
@@ -7,26 +7,6 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 const App = (props) => {
 
-  let posts = [
-    {id: 1, message: 'It\'s my fist post', likesCount: '100'},
-    {id: 2, message: 'Hi! How are you?', likesCount: '11'},
-    {id: 3, message: 'Stub / mock post', likesCount: '12'},
-    {id: 4, message: 'NEW ONE', likesCount: '22'}
-  ]
-
-  let dialogs = [
-    {id: 1, name: 'Andrew'},
-    {id: 2, name: 'Nick'},
-    {id: 3, name: 'Sally'},
-    {id: 4, name: 'Sarah'},
-  ]
-
-  let messages = [
-    {id: 1, message: 'Hello'},
-    {id: 2, message: 'What\'s up?'},
-    {id: 3, message: 'How is your studying?'}
-  ]
-
   return (
     <Router>
       <div className={"app-wrapper"}>
@@ -34,8 +14,8 @@ const App = (props) => {
         <Navbar/>
         <div className={"app-wrapper-content "}>
           <Routes>
-            <Route path="/dialogs/*" element={<Dialogs dialogs={dialogs} messages={messages} />} />
-            <Route exact path="/profile" element={<Profile posts={posts} />} />
+            <Route path="/dialogs/*" element={<Dialogs dialogs={props.dialogs} messages={props.messages} />} />
+            <Route exact path="/profile" element={<Profile posts={props.posts} />} />
           </Routes>
         </div>
       </div>
