5bbe633ad1ed2b11a272ead45e0ea49cd5e3da6e
andrey <auledovskih@gmail.com>
2022-06-16 00:26:45 +0300

    add callback

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
@@ -5,15 +6,21 @@ const MyPosts = (props) => {
 
   let postsElements = props.posts.map( p => <Post id={p.id} message={p.message} likesCount={p.likesCount} />)
 
+  let textElement = React.createRef();
+
+  let addPost = () => {
+    alert(textElement.current.value)
+  }
+
   return (
     <div className={s.myPosts}>
       My posts
       <div>
         <div>
-          <textarea/>
+          <textarea ref={textElement} />
         </div>
         <div>
-          <button>Add post</button>
+          <button onClick={addPost} >Add post</button>
         </div>
       </div>
       <div>
